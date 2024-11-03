112e1d2ff188de1e8244f5421f91a7f721dfad98
Ines Martins <inesmartins01@gmail.com>
2024-11-03 23:04:59 +0000

    add drag and drop and shake

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,62 +15,132 @@
  */
 package com.example.diceroller
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : ComponentActivity(), SensorEventListener {
+    private var linearAcceleration = FloatArray(3) { 0f }
+    private var lastShakeTime: Long = 0
+    private val shakeThreshold = 2.7f
+    private var currentResult = mutableStateOf(1)
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI)
+
         setContent {
             DiceRollerTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DiceRollerApp()
+                    DiceRollerApp (currentResult.value)
                 }
             }
         }
     }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val gravity = FloatArray(3) { 0f }
+        if (event != null) {
+            val alpha = 0.8f
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
+
+            linearAcceleration[0] = event.values[0] - gravity[0]
+            linearAcceleration[1] = event.values[1] - gravity[1]
+            linearAcceleration[2] = event.values[2] - gravity[2]
+
+            val magnitude = sqrt(
+                linearAcceleration[0] * linearAcceleration[0] +
+                        linearAcceleration[1] * linearAcceleration[1] +
+                        linearAcceleration[2] * linearAcceleration[2]
+            )
+
+            if (magnitude > shakeThreshold) {
+                val currentTime = System.currentTimeMillis()
+                if (currentTime - lastShakeTime > 5000) {
+                    lastShakeTime = currentTime
+                    rollDice { result ->
+                        currentResult.value = result
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
 }
 
-@Preview
-@Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
+
+fun rollDice(callback: (Int) -> Unit) {
+    val result = (1..6).random()
+    callback(result)
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf( 1) }
-    val imageResource = when(result) {
+fun DiceRollerApp(value: Int) {
+    DiceWithButtonAndImage(value)
+}
+
+@Composable
+fun DiceWithButtonAndImage(rollDiceCallback: Int) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        DiceDragAndDrop(numberToDisplay = rollDiceCallback)
+    }
+}
+
+@Composable
+fun DiceDragAndDrop(numberToDisplay: Int, modifier: Modifier = Modifier) {
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+    Log.i("Rolls", numberToDisplay.toString())
+
+    val imageResource = when (numberToDisplay) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -78,13 +148,19 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(imageResource), contentDescription = result.toString())
-        
-        Button(
-            onClick = { result = (1..6).random() },
-        ) {
-            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
+
+    Box(modifier = modifier.pointerInput(Unit) {
+        detectDragGestures { change, dragAmount ->
+            change.consume()
+            offsetX += dragAmount.x
+            offsetY += dragAmount.y
         }
+    }
+    ) {
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = numberToDisplay.toString(),
+            modifier = modifier.offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+        )
     }
 }
